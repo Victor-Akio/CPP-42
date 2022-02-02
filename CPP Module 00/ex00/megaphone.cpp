8acc19b42d452d0ce097a7fd49e49303333b3b4c
vminomiy <vminomiy@student.42sp.org.br>
2022-02-02 23:46:20 +0100

    first megaphone

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -6,15 +6,26 @@
 /*   By: vminomiy <vminomiy@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 23:11:24 by vminomiy          #+#    #+#             */
-/*   Updated: 2021/10/05 23:23:34 by vminomiy         ###   ########.fr       */
+/*   Updated: 2022/01/28 18:36:33 by vminomiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <iostream>
+#include <cstring>
 
-int
-main(void)
+int	main(int argc, char **argv)
+
 {
-	printf("Hellow World!\n");
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (--argc) {
+		argv++;
+		for (size_t i = 0; i < strlen(*argv); i++)
+			(*argv)[i] = std::toupper((*argv)[i]);
+		std::cout << *argv << " ";
+	}
+	std::cout << std::endl;
 	return (0);
 }
