218fd7b58d09d2c81a1f4dab8e6fa0524a70de68
vminomiy <vminomiy@student.42sp.org.br>
2022-03-15 21:38:53 +0100

    last adjustments to cpp03-02

--- CPP_Module_03/ex02/main.cpp
+++ CPP_Module_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vminomiy <vminomiy@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 01:17:38 by vminomiy          #+#    #+#             */
-/*   Updated: 2022/03/15 01:35:06 by vminomiy         ###   ########.fr       */
+/*   Updated: 2022/03/15 21:38:27 by vminomiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	claptrap(void) {
 	{
 		ClapTrap	ClapTrap;
 
+		std::cout << "[Attack until gets exhausted]" << std::endl;
 		for (int i = 0; i < 11; i++) {
 			ClapTrap.attack("the Air");
 		}
@@ -25,6 +26,7 @@ void	claptrap(void) {
 	{
 		ClapTrap	ClapTrap;
 
+		std::cout << "[repair until gets exuasted]" << std::endl;
 		ClapTrap.takeDamage(9);
 		for (int i = 0; i < 11; i++) {
 			ClapTrap.beRepaired(1);
@@ -33,6 +35,7 @@ void	claptrap(void) {
 	{
 		ClapTrap	ClapTrap;
 
+		std::cout << "[Dies...]" << std::endl;
 		ClapTrap.takeDamage(11);
 		ClapTrap.takeDamage(1);
 	}
@@ -42,6 +45,7 @@ void	scavtrap(void) {
 	{
 		ScavTrap	ScavTrap;
 
+		std::cout << "[Attack until gets exhausted]" << std::endl;
 		for (int i = 0; i < 51; i++) {
 			ScavTrap.attack("anything coming");
 		}
@@ -49,6 +53,7 @@ void	scavtrap(void) {
 	{
 		ScavTrap	ScavTrap;
 
+		std::cout << "[repair until gets exuasted]" << std::endl;
 		ScavTrap.takeDamage(99);
 		for (int i = 0; i < 51; i++) {
 			ScavTrap.beRepaired(1);
@@ -57,12 +62,14 @@ void	scavtrap(void) {
 	{
 		ScavTrap	ScavTrap;
 
+		std::cout << "[Dies...]" << std::endl;
 		ScavTrap.takeDamage(101);
 		ScavTrap.takeDamage(1);
 	}
 	{
 		ScavTrap	ScavTrap;
 
+		std::cout << "[Guard the gate]" << std::endl;
 		ScavTrap.guardGate();
 	}
 }
@@ -71,6 +78,7 @@ void	fragtrap(void) {
 	{
 		FragTrap	FragTrap;
 
+		std::cout << "[Attack until gets exhausted]" << std::endl;
 		for (int i = 0; i < 101; i++) {
 			FragTrap.attack("Barrels");
 		}
@@ -78,6 +86,7 @@ void	fragtrap(void) {
 	{
 		FragTrap	FragTrap;
 
+		std::cout << "[repair until gets exuasted]" << std::endl;
 		FragTrap.takeDamage(99);
 		for (int i = 0; i < 101; i++) {
 			FragTrap.beRepaired(1);
@@ -86,18 +95,20 @@ void	fragtrap(void) {
 	{
 		FragTrap	FragTrap;
 
+		std::cout << "[Dies...]" << std::endl;
 		FragTrap.takeDamage(100);
 		FragTrap.takeDamage(10);
 	}
 	{
 		FragTrap	FragTrap;
 
+		std::cout << "[Ask for High Fives.]" << std::endl;
 		FragTrap.highFivesGuys();
 	}
 }
 
 void	invArg(void) {
-	std::cout << "Error - Invalid arguments." << std::endl;
+	std::cout << "Error - Invalid argument." << std::endl;
 	std::cout << "Please, use the following syntax: \"./fragtrap <model_number>\"" << std::endl;
 	std::cout << "ClapTrap is 0, ScavTrap is 1 and FragTrap is 2." << std::endl;
 }
